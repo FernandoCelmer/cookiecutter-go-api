4a974f05879e90428e234ddd2d5a59cdb193c74a
FernandoCelmer <email@fernandocelmer.com>
2024-03-02 19:45:08 -0300

    ⚙️ FEATURE: CookieCutter

---
+++ {{cookiecutter.project_slug}}/cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	web "github.com/FernandoCelmer/api.go/src"
+)
+
+func main() {
+	app := web.NewApp(
+		web.Title("API"),
+		web.Description("Minimalist API Test"),
+		web.Version("0.1.0"),
+	)
+
+	app.Get("/item", itemHandler)
+
+	app.Run(
+		web.Host("127.0.0.1"),
+		web.Port(8080),
+	)
+}
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func itemHandler(w http.ResponseWriter, r *http.Request) {
+	data := Response{Message: "Item"}
+	response, _ := json.Marshal(data)
+	fmt.Fprintf(w, string(response))
+}
---
+++ {{cookiecutter.project_slug}}/go.mod
@@ -0,0 +1,5 @@
+module api.go/main
+
+go 1.22.0
+
+require github.com/FernandoCelmer/api.go v0.1.0
